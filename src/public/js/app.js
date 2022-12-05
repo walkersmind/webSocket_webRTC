4d89e378682333ce76f2cf0ef7672ed2d3b02a6d
Jeong <jeong@HwiJeongs-MacBook-Pro.local>
2022-12-05 12:45:18 +0900

    Feature: webSocket message receive

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,29 @@
-alert("Hello!");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server.");
+});
+
+// 전송 메시지 리스트로 추가하기
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from the server.");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-// server.js
-
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -10,4 +10,19 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
 
-app.listen(8000);
+const server = http.createServer(app);
+const webSocketServer = new WebSocket.Server({ server });
+
+const sockets = []; // sockets 배열 생성
+
+webSocketServer.on("connection", (socket) => {
+  sockets.push(socket); // 각 socket을 배열에 추가해주기
+  console.log("Connected to Browser.");
+  socket.on("open", () => console.log("Connected to the Browser"));
+  socket.on("message", (message) => {
+    sockets.forEach((eachSocket) => eachSocket.send(message.toString()));
+  }); // 각 소켓으로부터 수신한 메시지를 에코잉해주기
+  socket.on("close", () => console.log("Disconnectd from the Browser"));
+});
+
+server.listen(8000);
